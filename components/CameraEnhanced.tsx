190814b22b93be3c6b37690f8dd617af7c83c2b6
elsy0111 <kpp0193@gmail.com>
2025-05-10 21:20:37 +0900

    complete preview and shutter button

---
+++ components/CameraEnhanced.tsx
@@ -0,0 +1,71 @@
+// components/CameraEnhanced.tsx
+"use client";
+
+import Webcam from "react-webcam";
+
+import React, { useCallback, useRef, useState } from "react";
+
+const videoConstraints = {
+  facingMode: "user",
+};
+export function Nav({ capture }: { capture: () => void }) {
+  return (
+    <div className="absolute bottom-0 w-full px-10 mb-3 z-1">
+      <div className="flex items-center justify-between gap-4">
+        <div className="w-20 h-10 bg-yellow-500 rounded-lg flex items-center justify-between px-4">
+          <div className="w-2 h-2 bg-white rounded-full"></div>
+          <div className="w-2 h-2 bg-white rounded-full"></div>
+          <div className="w-2 h-2 bg-white rounded-full"></div>
+        </div>
+        <button
+          className="h-20 w-20 bg-white rounded-full flex items-center justify-center"
+          onClick={capture}
+        >
+          <div className="h-18 w-18 rounded-full bg-black flex items-center justify-center">
+            <div className="h-15 w-15 rounded-full bg-white"></div>
+          </div>
+        </button>
+        <div className="w-20 h-10 bg-red-500 rounded-lg flex items-center justify-between px-4">
+          <div className="w-2 h-2 bg-white rounded-full"></div>
+          <div className="w-2 h-2 bg-white rounded-full"></div>
+          <div className="w-2 h-2 bg-white rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function ShowImg({ image }: { image: string | null }) {
+  return (
+    <div className="absolute bottom-0 z-0">
+      <div></div>
+      {image && <img src={image} alt="Taken photo" className="rounded-lg" />}
+    </div>
+  );
+}
+
+export default function CameraEnhanced() {
+  const webcamRef = useRef<any>(null);
+  const [image, setImage] = useState<string | null>(null);
+
+  const capture = useCallback(() => {
+    if (webcamRef.current) {
+      const image = webcamRef.current.getScreenshot();
+      setImage(image);
+    }
+  }, [webcamRef]);
+
+  return (
+    <div className="h-screen bg-gray-500">
+      <Webcam
+        audio={false}
+        screenshotFormat="image/jpeg"
+        videoConstraints={videoConstraints}
+        className="h-full w-screen object-cover"
+        ref={webcamRef}
+      ></Webcam>
+      <Nav capture={capture} />
+      <ShowImg image={image} />
+    </div>
+  );
+}
