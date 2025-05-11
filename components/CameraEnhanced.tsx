864ad2c22fb1186d2673b7e4d88f4322f7c8b673
elsy0111 <kpp0193@gmail.com>
2025-05-11 09:08:00 +0900

    add change camera button and font awesome icon

--- components/CameraEnhanced.tsx
+++ components/CameraEnhanced.tsx
@@ -6,17 +6,26 @@ import Image from "next/image";
 
 import React, { useCallback, useRef, useState } from "react";
 
-const videoConstraints = {
-  facingMode: "user",
-};
-export function Nav({ capture }: { capture: () => void }) {
+// fontawesome icon
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
+
+export function Nav({
+  capture,
+  setFacingMode,
+  facingMode,
+}: {
+  capture: () => void;
+  setFacingMode: (mode: string) => void;
+  facingMode: string;
+}) {
   return (
     <div className="absolute bottom-0 w-full px-10 mb-3 z-1">
       <div className="flex items-center justify-between gap-4">
-        <div className="w-20 h-10 bg-yellow-500 rounded-lg flex items-center justify-between px-4">
-          <div className="w-2 h-2 bg-white rounded-full"></div>
-          <div className="w-2 h-2 bg-white rounded-full"></div>
-          <div className="w-2 h-2 bg-white rounded-full"></div>
+        {/* Upload button */}
+        <div className="w-20 h-10 bg-yellow-500 rounded-lg flex items-center justify-center px-4">
+          <FontAwesomeIcon icon={faUpload} className="text-white text-2xl" />
         </div>
         <button
           className="h-20 w-20 bg-white rounded-full flex items-center justify-center"
@@ -26,11 +35,19 @@ export function Nav({ capture }: { capture: () => void }) {
             <div className="h-15 w-15 rounded-full bg-white"></div>
           </div>
         </button>
-        <div className="w-20 h-10 bg-red-500 rounded-lg flex items-center justify-between px-4">
-          <div className="w-2 h-2 bg-white rounded-full"></div>
-          <div className="w-2 h-2 bg-white rounded-full"></div>
-          <div className="w-2 h-2 bg-white rounded-full"></div>
-        </div>
+        <button
+          className="w-20 h-10 bg-red-500 rounded-lg flex items-center justify-center px-4"
+          // change inside outside camera
+          onClick={() => {
+            if (facingMode === "user") {
+              setFacingMode("environment");
+            } else {
+              setFacingMode("user");
+            }
+          }}
+        >
+          <FontAwesomeIcon icon={faCamera} className="text-white text-2xl" />
+        </button>
       </div>
     </div>
   );
@@ -56,6 +73,7 @@ export function ShowImg({ image }: { image: string | null }) {
 export default function CameraEnhanced() {
   const webcamRef = useRef<Webcam>(null);
   const [image, setImage] = useState<string | null>(null);
+  const [facingMode, setFacingMode] = useState<string>("environment");
 
   const capture = useCallback(() => {
     if (webcamRef.current) {
@@ -63,6 +81,10 @@ export default function CameraEnhanced() {
       setImage(image);
     }
   }, [webcamRef]);
+
+  const videoConstraints = {
+    facingMode: facingMode,
+  };
 
   return (
     <div className="h-svh bg-gray-500">
@@ -74,7 +96,11 @@ export default function CameraEnhanced() {
         ref={webcamRef}
         mirrored={true}
       ></Webcam>
-      <Nav capture={capture} />
+      <Nav
+        capture={capture}
+        facingMode={facingMode}
+        setFacingMode={setFacingMode}
+      />
       <ShowImg image={image} />
     </div>
   );
