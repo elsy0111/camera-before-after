b5f4934d977e4ee1ea3d2f2a45943d5a07b48139
elsy0111 <kpp0193@gmail.com>
2025-05-11 10:33:45 +0900

    object contain and quarity up

--- components/CameraEnhanced.tsx
+++ components/CameraEnhanced.tsx
@@ -4,147 +4,148 @@
 import Webcam from "react-webcam";
 import Image from "next/image";
 
-import React, { useCallback, useRef, useState } from "react";
+import React, {useCallback, useRef, useState} from "react";
 
 // fontawesome icon
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRotate } from "@fortawesome/free-solid-svg-icons";
-import { faUpload } from "@fortawesome/free-solid-svg-icons";
-import { faArrowRightArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faRotate} from "@fortawesome/free-solid-svg-icons";
+import {faUpload} from "@fortawesome/free-solid-svg-icons";
+import {faArrowRightArrowLeft} from "@fortawesome/free-solid-svg-icons";
 
 export function Nav({
-  capture,
-  facingMode,
-  setFacingMode,
-  mirrored,
-  setMirrored,
-  setImage,
+	capture,
+	facingMode,
+	setFacingMode,
+	mirrored,
+	setMirrored,
+	setImage,
 }: {
-  capture: () => void;
-  facingMode: string;
-  setFacingMode: (mode: string) => void;
-  mirrored: boolean;
-  setMirrored: (mode: boolean) => void;
-  setImage: (image: string | null) => void;
+	capture: () => void;
+	facingMode: string;
+	setFacingMode: (mode: string) => void;
+	mirrored: boolean;
+	setMirrored: (mode: boolean) => void;
+	setImage: (image: string | null) => void;
 }) {
-  // function to handle Upload
-  const handleUpload = () => {
-    const input = document.createElement("input");
-    input.type = "file";
-    input.accept = "image/*";
-    input.onchange = (event) => {
-      const file = (event.target as HTMLInputElement).files?.[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onloadend = () => {
-          setImage(reader.result as string);
-        };
-        reader.readAsDataURL(file);
-      }
-    };
-    input.click();
-  };
+	// function to handle Upload
+	const handleUpload = () => {
+		const input = document.createElement("input");
+		input.type = "file";
+		input.accept = "image/*";
+		input.onchange = (event) => {
+			const file = (event.target as HTMLInputElement).files?.[0];
+			if (file) {
+				const reader = new FileReader();
+				reader.onloadend = () => {
+					setImage(reader.result as string);
+				};
+				reader.readAsDataURL(file);
+			}
+		};
+		input.click();
+	};
 
-  return (
-    <div className="absolute bottom-0 w-full px-10 mb-3 z-1">
-      <div className="grid grid-cols-5 items-center gap-10">
-        {/* Upload button */}
-        <button
-          className="h-10 bg-yellow-500 rounded-lg flex items-center justify-center px-4 col-span-2"
-          onClick={handleUpload}
-        >
-          <FontAwesomeIcon icon={faUpload} className="text-white text-2xl" />
-        </button>
-        {/* Capture button */}
-        <div className="justify-self-center">
-          <button
-            className="h-20 w-20 bg-white rounded-full flex items-center justify-center"
-            onClick={capture}
-          >
-            <div className="h-18 w-18 rounded-full bg-black flex items-center justify-center">
-              <div className="h-15 w-15 rounded-full bg-white"></div>
-            </div>
-          </button>
-        </div>
-        {/* Change camera button */}
-        <button
-          className="h-10 bg-red-500 rounded-lg flex items-center justify-center px-4"
-          // change inside outside camera
-          onClick={() => {
-            setFacingMode(facingMode === "user" ? "environment" : "user");
-          }}
-        >
-          <FontAwesomeIcon icon={faRotate} className="text-white text-2xl" />
-        </button>
-        {/* Mirror button */}
-        <button
-          className="h-10 bg-blue-500 rounded-lg flex items-center justify-center px-4"
-          onClick={() => {
-            setMirrored(!mirrored);
-          }}
-        >
-          <FontAwesomeIcon
-            icon={faArrowRightArrowLeft}
-            className="text-white text-2xl"
-          />
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="absolute bottom-0 w-full px-10 mb-3 z-1">
+			<div className="grid grid-cols-5 items-center gap-10">
+				{/* Upload button */}
+				<button
+					className="h-10 bg-yellow-500 rounded-lg flex items-center justify-center px-4 col-span-2"
+					onClick={handleUpload}
+				>
+					<FontAwesomeIcon icon={faUpload} className="text-white text-2xl" />
+				</button>
+				{/* Capture button */}
+				<div className="justify-self-center">
+					<button
+						className="h-20 w-20 bg-white rounded-full flex items-center justify-center"
+						onClick={capture}
+					>
+						<div className="h-18 w-18 rounded-full bg-black flex items-center justify-center">
+							<div className="h-15 w-15 rounded-full bg-white"></div>
+						</div>
+					</button>
+				</div>
+				{/* Change camera button */}
+				<button
+					className="h-10 bg-red-500 rounded-lg flex items-center justify-center px-4"
+					// change inside outside camera
+					onClick={() => {
+						setFacingMode(facingMode === "user" ? "environment" : "user");
+					}}
+				>
+					<FontAwesomeIcon icon={faRotate} className="text-white text-2xl" />
+				</button>
+				{/* Mirror button */}
+				<button
+					className="h-10 bg-blue-500 rounded-lg flex items-center justify-center px-4"
+					onClick={() => {
+						setMirrored(!mirrored);
+					}}
+				>
+					<FontAwesomeIcon
+						icon={faArrowRightArrowLeft}
+						className="text-white text-2xl"
+					/>
+				</button>
+			</div>
+		</div>
+	);
 }
 
-export function ShowImg({ image }: { image: string | null }) {
-  return (
-    <div className="absolute top-0 right-0 z-0">
-      {image && (
-        <Image
-          src={image}
-          height={0}
-          width={200}
-          alt="Taken photo"
-          className="rounded-bl-md shadow-lg"
-        />
-      )}
-    </div>
-  );
+export function ShowImg({image}: {image: string | null}) {
+	return (
+		<div className="absolute top-0 right-0 z-0">
+			{image && (
+				<Image
+					src={image}
+					height={0}
+					width={200}
+					alt="Taken photo"
+					className="rounded-bl-md shadow-lg"
+				/>
+			)}
+		</div>
+	);
 }
 
 export default function CameraEnhanced() {
-  const webcamRef = useRef<Webcam>(null);
-  const [image, setImage] = useState<string | null>(null);
-  const [facingMode, setFacingMode] = useState<string>("environment");
-  const [mirrored, setMirrored] = useState<boolean>(false);
+	const webcamRef = useRef<Webcam>(null);
+	const [image, setImage] = useState<string | null>(null);
+	const [facingMode, setFacingMode] = useState<string>("environment");
+	const [mirrored, setMirrored] = useState<boolean>(false);
 
-  const capture = useCallback(() => {
-    if (webcamRef.current) {
-      const image = webcamRef.current.getScreenshot();
-      setImage(image);
-    }
-  }, [webcamRef]);
+	const capture = useCallback(() => {
+		if (webcamRef.current) {
+			const image = webcamRef.current.getScreenshot();
+			setImage(image);
+		}
+	}, [webcamRef]);
 
-  const videoConstraints = {
-    facingMode: facingMode,
-  };
+	const videoConstraints = {
+		facingMode: facingMode,
+	};
 
-  return (
-    <div className="h-svh bg-gray-500">
-      <Webcam
-        audio={false}
-        screenshotFormat="image/jpeg"
-        videoConstraints={videoConstraints}
-        className="h-full w-full object-cover"
-        ref={webcamRef}
-        mirrored={mirrored}
-      ></Webcam>
-      <Nav
-        capture={capture}
-        facingMode={facingMode}
-        setFacingMode={setFacingMode}
-        mirrored={mirrored}
-        setMirrored={setMirrored}
-        setImage={setImage}
-      />
-      <ShowImg image={image} />
-    </div>
-  );
+	return (
+		<div className="h-svh bg-gray-500">
+			<Webcam
+				audio={false}
+				screenshotFormat="image/jpeg"
+				videoConstraints={videoConstraints}
+				className="h-full w-full object-contain"
+				ref={webcamRef}
+				mirrored={mirrored}
+				screenshotQuality={1}
+			></Webcam>
+			<Nav
+				capture={capture}
+				facingMode={facingMode}
+				setFacingMode={setFacingMode}
+				mirrored={mirrored}
+				setMirrored={setMirrored}
+				setImage={setImage}
+			/>
+			<ShowImg image={image} />
+		</div>
+	);
 }
